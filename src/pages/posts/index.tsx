9c98290e40801e768772aea9951deea9795e6ee8
shugo1125 <iamsyu25@icloud.com>
2025-04-13 11:42:02 +0900

    記事一覧を作成

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,44 @@
+// src/pages/posts/index.tsx
+
+import { GetStaticProps, NextPage } from "next";
+
+// ✅ 投稿の型
+type Post = {
+  id: number;
+  title: string;
+  date: string;
+};
+
+// ✅ propsの型
+type HomeProps = {
+  allPostsData: Post[];
+};
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  return {
+    props: {
+      allPostsData: [
+        { id: 1, title: "First Post", date: "2024-01-01" },
+        { id: 2, title: "Second Post", date: "2024-01-02" },
+        { id: 3, title: "Third Post", date: "2024-01-03" },
+      ],
+    },
+  };
+};
+
+const Home: NextPage<HomeProps> = ({ allPostsData }) => {
+  return (
+    <>
+      <h1>記事一覧</h1>
+      {allPostsData.map(({ id, title, date }) => (
+        <div key={id}>
+          <p>
+            date: {date}, title: {title}
+          </p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Home;
