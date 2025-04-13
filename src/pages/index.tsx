c9fd45ffeb12401c9db46fd75cf699edfd700a79
shugo1125 <iamsyu25@icloud.com>
2025-04-13 11:02:55 +0900

    CSSを付与

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
+import styles from "@/styles/Home.module.css";
+import localFont from "next/font/local";
 import Head from "next/head";
 import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 
 const geistSans = localFont({
--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import Image from "next/image";
+import styles from "./first-post.module.css";
+
 const FirstPost: NextPage = () => {
   return (
     <>
-      <h1>Hello,first post!</h1>
+      <h1 className={styles.title}>Hello,first post!</h1>
       <Link href="../">戻る</Link>
+
+      <Image src="/sakura.jpeg" alt="sakura" width={100} height={100} />
     </>
-  )
-}
+  );
+};
 
 export default FirstPost;
-
